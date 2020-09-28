ab71e633cbea2f7160056b7689cc74d195d35392
jasonf <jasonf@pop-os.localdomain>
2020-09-28 11:57:41 -0500

    Added sidenav unit test for toggle. Update Coin card component name to be coin table component.

---
+++ src/app/components/sidenav/home/coin-card/coin-table.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoinTableComponent } from './coin-table.component';
+
+describe('CoinCardComponent', () => {
+  // let component: CoinTableComponent;
+  // let fixture: ComponentFixture<CoinTableComponent>;
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [CoinTableComponent],
+  //   }).compileComponents();
+  // });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(CoinTableComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
--- src/app/directives/value-change.directive.ts
+++ src/app/directives/value-change.directive.ts
@@ -7,7 +7,6 @@ export class ValueChangeDirective {
   constructor(private el: ElementRef) {}
 
   ngAfterContentInit() {
-    console.log(this.el.nativeElement);
     const color =
       this.el.nativeElement.innerText.substr(0, 1) === '-' ? 'red' : 'green';
     this.el.nativeElement.style.color = color;
