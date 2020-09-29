4b02d23978df3571e8905289911b753a454f5881
jasonf <jasonf@pop-os.localdomain>
2020-09-28 20:17:57 -0500

    updated all unit tests

--- src/app/services/coin.service.spec.ts
+++ src/app/services/coin.service.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CoinService } from './coin.service';
 
 describe('CoinService', () => {
-  // let service: CoinService;
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(CoinService);
-  // });
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  let service: CoinService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(CoinService);
+  });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
