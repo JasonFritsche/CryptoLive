da1e6e405d683bc8271aa57b558702cafc25643e
jason <jason@jasons-thinkpad.attlocal.net>
2020-10-12 20:48:03 -0500

    added search to coin table

--- src/app/components/sidenav/home/coin-table/coin-table.component.ts
+++ src/app/components/sidenav/home/coin-table/coin-table.component.ts
@@ -6,12 +6,21 @@ import { Router } from '@angular/router';
 import { CoinService } from 'src/app/services/coin.service';
 import { CoinInfoComponent } from './coin-info/coin-info.component';
 
+export interface Coin {
+  icon: string;
+  name: string;
+  volume: string;
+  price: string;
+  change: string;
+}
+
 @Component({
   selector: 'app-coin-table',
   templateUrl: './coin-table.component.html',
   styleUrls: ['./coin-table.component.scss'],
 })
 export class CoinTableComponent implements OnInit, AfterViewInit {
+  public dataSource = new MatTableDataSource();
   public isLoaded: boolean = false;
   @ViewChild(MatSort) sort: MatSort;
   public displayedColumns: string[] = [
@@ -21,8 +30,6 @@ export class CoinTableComponent implements OnInit, AfterViewInit {
     'price',
     'change',
   ];
-
-  public dataSource = new MatTableDataSource();
 
   constructor(
     private coinService: CoinService,
@@ -34,10 +41,13 @@ export class CoinTableComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.isLoaded = false;
     this.refresh();
+    this.dataSource.filterPredicate = function(data: Coin, filter: string): boolean {
+      return data.name.toLowerCase().includes(filter);
+    };
   }
 
   public refresh(): void {
-    this.coinService.currentCoinData().subscribe((res) => {
+    this.coinService.currentCoinData().subscribe((res: Coin[]) => {
       this.dataSource.data = res;
       this.isLoaded = true;
     });
@@ -52,7 +62,13 @@ export class CoinTableComponent implements OnInit, AfterViewInit {
     const dialogRef = this.dialog.open(CoinInfoComponent, {
       data: el,
       width: '70vw',
+      panelClass: 'coin-info-dialog',
     });
+  }
+
+  public searchTable(searchTerm: string): void {
+    this.dataSource.filter = searchTerm.trim().toLowerCase();
+    console.log(this.dataSource.filter);
   }
 
   ngAfterViewInit() {
